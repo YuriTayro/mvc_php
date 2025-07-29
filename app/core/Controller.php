800f4f82ae9eb81404e449b79f455509ce2d81d4
Yuri Tayro <96271669+YuriTayro@users.noreply.github.com>
2025-07-28 22:28:51 -0300

    Segundo commit

--- app/core/Controller.php
+++ app/core/Controller.php
@@ -2,6 +2,7 @@
 
 class Controller{
     protected function view($view, $viewData = []){
+        extract($viewData);
         $viewFile = __DIR__ . '/../views/'.$view.'.php';
         if(!file_exists($viewFile)){
             throw new Exception('View file not found: '. $viewFile);
